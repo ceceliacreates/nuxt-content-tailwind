1ce8e46abc77fbdcc41b92d1196f9a6c10edec8d
ceceliacreates <ceceliacreates@gmail.com>
2024-03-13 11:57:27 -0400

    start styling, will add more

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="bg-gradient-to-r from-[#cdffd8] to-[#94b9ff] min-h-screen">
     <NavBar />
     <NuxtPage />
   </div>
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,8 +1,8 @@
 <template>
-    <div>
-        <NuxtLink to="/">Home</NuxtLink>
-        <NuxtLink to="/about">About</NuxtLink>
-        <NuxtLink to="/blog">Blog</NuxtLink>
-        <NuxtLink to="/projects">Projects</NuxtLink>
+    <div class="flex justify-between max-w-sm md:max-w-lg px-4 py-4 sm:px-8 mx-auto text-[#2a3d45]">
+        <NuxtLink to="/" exact-active-class="font-bold">Home</NuxtLink>
+        <NuxtLink to="/about" active-class="font-bold" >About</NuxtLink>
+        <NuxtLink to="/blog" active-class="font-bold">Blog</NuxtLink>
+        <NuxtLink to="/projects" active-class="font-bold">Projects</NuxtLink>
     </div>
   </template>
