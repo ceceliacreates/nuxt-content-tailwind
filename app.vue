0849b7f931f04f4c49df068be6fdd2bd67a0275e
ceceliacreates <ceceliacreates@gmail.com>
2024-03-26 14:41:10 -0400

    updates favicon and SEO meta details

--- app.vue
+++ app.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Cecelia Martinez',
+  ogTitle: 'Cecelia Martinez',
+  description: 'events, blog, and projects from Lead Developer Advocate Cecelia Martinez',
+  ogDescription: 'events, blog, and projects from Lead Developer Advocate Cecelia Martinez',
+  ogUrl: 'https://ceceliacreates.com',
+  ogSiteName: 'Cecelia Martinez',
+  ogType: 'website',
+  ogImage: 'https://ceceliacreates.com/images/og-image.png',
+  twitterSite: '@ceceliacreates',
+  twitterCard: 'summary_large_image',
+})
+</script>
+
 <template>
   <div class="min-h-screen bg-white">
     <NavBar />
